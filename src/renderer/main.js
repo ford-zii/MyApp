83306938839d8b8c678140a7f09416b5098ce88b
JittimaSrisang <37680256+JittimaSrisang@users.noreply.github.com>
2019-03-10 19:14:46 +0700

    change
    change

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,10 +5,10 @@ import 'element-ui/lib/theme-chalk/index.css';
 import App from './App'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify'
+
 
 Vue.use(ElementUI);
-Vue.use(Vuetify)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
