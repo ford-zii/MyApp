5e4589bed12b2e49be96dd44a5723582229ad0f1
Suwantha21 <Suwanthagib@github.com>
2019-04-11 13:53:11 +0700

    Refactor

--- src/renderer/components/Login.vue
+++ src/renderer/components/Login.vue
@@ -1,15 +1,25 @@
 <template>
   <!--<div style="width: 200px " ></div>-->
 
-  <el-row>
-    <el-col :span="12" :offset="5">
+  <el-row  style="margin: 240px" >
+    <el-col :span="16" :offset="6">
       <el-form :model="form" status-icon  ref="form" label-width="120px" class="demo-ruleForm">
         <i class="fas fa-id-card"></i>
-        <el-form-item label="User" prop="user" >
-          <el-input type="text" v-model="form.username" autocomplete="off" ></el-input>
+        <el-form-item label="User"
+                      prop="user" >
+                      <!--:rules="[-->
+      <!--{ required: true, message: 'Please input email address', trigger: 'blur' },-->
+      <!--{ type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }-->
+    <!--]" >-->
+
+          <el-input placeholder="user" type="text" v-model="form.username" autocomplete="off" ></el-input>
         </el-form-item>
         <el-form-item label="Password" prop="pass" >
-          <el-input type="password" v-model="form.password" autocomplete="off"></el-input>
+        <!--:rules="[-->
+        <!--{ required: true, message: 'Please input email address', trigger: 'blur' },-->
+        <!--{ type: 'email', message: 'Please input correct email address', trigger: ['blur', 'change'] }-->
+        <!--]" >-->
+          <el-input placeholder="password" type="password" v-model="form.password" autocomplete="off"></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" @click="submitForm('form')">Login</el-button>
@@ -51,6 +61,11 @@
 
 </script>
 
-<style scoped>
+<style >
+  body {
+    background-color: lavender;
+    background-size: cover;
+
+  }
 
 </style>
