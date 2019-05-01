15b883d7e0865a083d5d244bf2394be6f801eb1e
Suwantha21 <Suwanthagib@github.com>
2019-05-01 21:17:10 +0700

    Change color menu

--- src/renderer/components/template.vue
+++ src/renderer/components/template.vue
@@ -10,22 +10,22 @@
                         <span>Member</span>
                     </el-menu-item>
                     <el-menu-item index="2" @click="goProduct()">
-                        <font-awesome-icon icon="store" size="lg"/>
+                        <font-awesome-icon icon="store" size="lg" style="color: #d7c8f4"/>
                         <span>Product</span>
                     </el-menu-item>
                     <el-menu-item index="3" @click="goStaff()">
-                        <font-awesome-icon icon="user" size="lg" />
+                        <font-awesome-icon icon="user" size="lg" style="color: #a8daf4" />
                         <span>Staff</span>
                     </el-menu-item>
-                    <el-menu-item index="4" @click="goSell()">
-                        <font-awesome-icon icon="dollar-sign" size="2x" />
+                    <el-menu-item index @click="goSell()">
+                        <font-awesome-icon icon="dollar-sign" size="2x" style="color: #f3b8d0" />
                         <!--<font-awesome-icon icon="shopping-cart" size="2x" />-->
 
                         <span> Sell</span>
                     </el-menu-item>
                     <el-submenu index="5">
                         <template slot="title">
-                            <font-awesome-icon icon="clipboard" size="lg"/>
+                            <font-awesome-icon icon="clipboard" size="lg" style="color: #f3b4a4"/>
                             <span>Management</span>
                         </template>
                         <el-menu-item-group>
@@ -39,7 +39,7 @@
 
                     </el-submenu>
                     <el-menu-item index="6" @click="logout()">
-                        <font-awesome-icon icon="sign-out-alt" size="lg"/>
+                        <font-awesome-icon icon="sign-out-alt" size="lg" style="color: #f34e68"/>
 
                         <!--<i class="el-icon-goods"></i>-->
                         <span>Logout</span>
